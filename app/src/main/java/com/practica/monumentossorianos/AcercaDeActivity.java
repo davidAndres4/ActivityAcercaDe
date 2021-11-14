8c8a2e2933a4c486ba453c119a85d2e1c05d392b
David <david.andres.otero@iesjulianmarias.es>
2021-11-15 00:39:30 +0100

    lanza nueva actividad pulsando botón Acerca de

---
+++ app/src/main/java/com/practica/monumentossorianos/AcercaDeActivity.java
@@ -0,0 +1,14 @@
+package com.practica.monumentossorianos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AcercaDeActivity extends Activity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
